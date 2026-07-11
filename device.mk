349869821c0a5e64981eb9b3a51cadd48ea45203
Andrey Sprynga <rtx4d@aospa.co>
2026-07-11 23:25:30 +0700

    trident: Downgrade SoundTrigger to 2.2
    Fixes 'Hey Google' command
    
    Change-Id: I7cefcd7e27174a51b01fcbb7b39b4a874558f9db

--- device.mk
+++ device.mk
@@ -89,7 +89,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl.smartisan:32 \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.service \
-    android.hardware.soundtrigger@2.3-impl:32 \
+    android.hardware.soundtrigger@2.2-impl:32 \
     audio.r_submix.default \
     audio.usbv2.default \
     libaudioroute.vendor \
